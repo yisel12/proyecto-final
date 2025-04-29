bf53b7e0bbfbb504916e06248601a1bb5ec0ae8b
yisel12 <yiselandrea122@gmail.com>
2025-04-28 23:07:55 -0500

    data

---
+++ js/data.js
@@ -0,0 +1,454 @@
+const productos = [
+  {
+    "nombre": "Chaleco de peso",
+    "categoria": "Fitness",
+    "imagen": "deporte.png",
+    "codigo": "jgq68mj258",
+    "precio": 233703,
+    "marca": "Puma",
+    "Color": "Naranja"
+  },
+  {
+    "nombre": "Cinta de correr",
+    "categoria": "Béisbol",
+    "imagen": "deporte.png",
+    "codigo": "ufi51kt721",
+    "precio": 444198,
+    "marca": "Decathlon",
+    "Color": "Morado"
+  },
+  {
+    "nombre": "Palo de golf",
+    "categoria": "Boxeo",
+    "imagen": "deporte.png",
+    "codigo": "gtc20yt586",
+    "precio": 154830,
+    "marca": "Everlast",
+    "Color": "Celeste"
+  },
+  {
+    "nombre": "Pelota de béisbol",
+    "categoria": "Boxeo",
+    "imagen": "deporte.png",
+    "codigo": "tew37fg051",
+    "precio": 21470,
+    "marca": "Wilson",
+    "Color": "Verde"
+  },
+  {
+    "nombre": "Cuerda para saltar",
+    "categoria": "Fútbol",
+    "imagen": "deporte.png",
+    "codigo": "hlu54rk399",
+    "precio": 220735,
+    "marca": "Asics",
+    "Color": "Gris"
+  },
+  {
+    "nombre": "Mancuernas",
+    "categoria": "Baloncesto",
+    "imagen": "deporte.png",
+    "codigo": "fgi36ih903",
+    "precio": 313663,
+    "marca": "Under Armour",
+    "Color": "Blanco"
+  },
+  {
+    "nombre": "Palo de golf",
+    "categoria": "Fitness",
+    "imagen": "deporte.png",
+    "codigo": "amw92fr470",
+    "precio": 172821,
+    "marca": "Everlast",
+    "Color": "Negro"
+  },
+  {
+    "nombre": "Gafas deportivas",
+    "categoria": "Golf",
+    "imagen": "deporte.png",
+    "codigo": "csy90ek304",
+    "precio": 119148,
+    "marca": "Asics",
+    "Color": "Blanco"
+  },
+  {
+    "nombre": "Rodillera",
+    "categoria": "Voleibol",
+    "imagen": "deporte.png",
+    "codigo": "dwt18rl954",
+    "precio": 482297,
+    "marca": "Nike",
+    "Color": "Amarillo"
+  },
+  {
+    "nombre": "Pesas",
+    "categoria": "Baloncesto",
+    "imagen": "deporte.png",
+    "codigo": "cdb13ar957",
+    "precio": 424910,
+    "marca": "Decathlon",
+    "Color": "Negro"
+  },
+  {
+    "nombre": "Rodillera",
+    "categoria": "Golf",
+    "imagen": "deporte.png",
+    "codigo": "jxh65uq629",
+    "precio": 424379,
+    "marca": "Spalding",
+    "Color": "Amarillo"
+  },
+  {
+    "nombre": "Pesas",
+    "categoria": "Boxeo",
+    "imagen": "deporte.png",
+    "codigo": "wce72ou291",
+    "precio": 92525,
+    "marca": "Puma",
+    "Color": "Blanco"
+  },
+  {
+    "nombre": "Red de voleibol",
+    "categoria": "Fútbol",
+    "imagen": "deporte.png",
+    "codigo": "kuj07kn132",
+    "precio": 22735,
+    "marca": "Spalding",
+    "Color": "Celeste"
+  },
+  {
+    "nombre": "Pesas",
+    "categoria": "Golf",
+    "imagen": "deporte.png",
+    "codigo": "quh29ma691",
+    "precio": 127436,
+    "marca": "Spalding",
+    "Color": "Rojo"
+  },
+  {
+    "nombre": "Pelota de béisbol",
+    "categoria": "Boxeo",
+    "imagen": "deporte.png",
+    "codigo": "fji09gh530",
+    "precio": 317957,
+    "marca": "Nike",
+    "Color": "Amarillo"
+  },
+  {
+    "nombre": "Gorro deportivo",
+    "categoria": "Golf",
+    "imagen": "deporte.png",
+    "codigo": "xvb66ap037",
+    "precio": 95804,
+    "marca": "Asics",
+    "Color": "Amarillo"
+  },
+  {
+    "nombre": "Pelota de béisbol",
+    "categoria": "Baloncesto",
+    "imagen": "deporte.png",
+    "codigo": "fzm56ln687",
+    "precio": 373834,
+    "marca": "Wilson",
+    "Color": "Morado"
+  },
+  {
+    "nombre": "Rodillera",
+    "categoria": "Boxeo",
+    "imagen": "deporte.png",
+    "codigo": "oyi87dk521",
+    "precio": 195944,
+    "marca": "Spalding",
+    "Color": "Morado"
+  },
+  {
+    "nombre": "Red de voleibol",
+    "categoria": "Béisbol",
+    "imagen": "deporte.png",
+    "codigo": "cxh83hn334",
+    "precio": 493945,
+    "marca": "Spalding",
+    "Color": "Negro"
+  },
+  {
+    "nombre": "Red de voleibol",
+    "categoria": "Béisbol",
+    "imagen": "deporte.png",
+    "codigo": "yjr86sm506",
+    "precio": 456138,
+    "marca": "Spalding",
+    "Color": "Amarillo"
+  },
+  {
+    "nombre": "Cuerda para saltar",
+    "categoria": "Baloncesto",
+    "imagen": "deporte.png",
+    "codigo": "oxt47pd960",
+    "precio": 352850,
+    "marca": "Wilson",
+    "Color": "Azul"
+  },
+  {
+    "nombre": "Gafas deportivas",
+    "categoria": "Voleibol",
+    "imagen": "deporte.png",
+    "codigo": "yul34og178",
+    "precio": 42335,
+    "marca": "Everlast",
+    "Color": "Negro"
+  },
+  {
+    "nombre": "Bicicleta estática",
+    "categoria": "Boxeo",
+    "imagen": "deporte.png",
+    "codigo": "rcj29zy669",
+    "precio": 386239,
+    "marca": "Wilson",
+    "Color": "Negro"
+  },
+  {
+    "nombre": "Gafas deportivas",
+    "categoria": "Baloncesto",
+    "imagen": "deporte.png",
+    "codigo": "hmv66bu496",
+    "precio": 107582,
+    "marca": "Reebok",
+    "Color": "Negro"
+  },
+  {
+    "nombre": "Casco de ciclismo",
+    "categoria": "Fitness",
+    "imagen": "deporte.png",
+    "codigo": "nrz00kr333",
+    "precio": 412767,
+    "marca": "Adidas",
+    "Color": "Amarillo"
+  },
+  {
+    "nombre": "Pesas",
+    "categoria": "Golf",
+    "imagen": "deporte.png",
+    "codigo": "smi59zo310",
+    "precio": 389586,
+    "marca": "Nike",
+    "Color": "Amarillo"
+  },
+  {
+    "nombre": "Raqueta de tenis",
+    "categoria": "Ciclismo",
+    "imagen": "deporte.png",
+    "codigo": "ngs52ie371",
+    "precio": 343921,
+    "marca": "Under Armour",
+    "Color": "Gris"
+  },
+  {
+    "nombre": "Pesas",
+    "categoria": "Ciclismo",
+    "imagen": "deporte.png",
+    "codigo": "nfi84rh847",
+    "precio": 169527,
+    "marca": "Decathlon",
+    "Color": "Amarillo"
+  },
+  {
+    "nombre": "Gafas deportivas",
+    "categoria": "Running",
+    "imagen": "deporte.png",
+    "codigo": "zob95ft380",
+    "precio": 311816,
+    "marca": "Spalding",
+    "Color": "Morado"
+  },
+  {
+    "nombre": "Cuerda para saltar",
+    "categoria": "Béisbol",
+    "imagen": "deporte.png",
+    "codigo": "lvp61kc179",
+    "precio": 218553,
+    "marca": "Spalding",
+    "Color": "Naranja"
+  },
+  {
+    "nombre": "Palo de golf",
+    "categoria": "Fitness",
+    "imagen": "deporte.png",
+    "codigo": "ses92re803",
+    "precio": 442774,
+    "marca": "Adidas",
+    "Color": "Negro"
+  },
+  {
+    "nombre": "Cuerda para saltar",
+    "categoria": "Béisbol",
+    "imagen": "deporte.png",
+    "codigo": "kga26vs571",
+    "precio": 52029,
+    "marca": "Adidas",
+    "Color": "Negro"
+  },
+  {
+    "nombre": "Red de voleibol",
+    "categoria": "Voleibol",
+    "imagen": "deporte.png",
+    "codigo": "vwp86en251",
+    "precio": 297736,
+    "marca": "Puma",
+    "Color": "Blanco"
+  },
+  {
+    "nombre": "Balón de fútbol",
+    "categoria": "Baloncesto",
+    "imagen": "deporte.png",
+    "codigo": "sxh59xp884",
+    "precio": 467372,
+    "marca": "Asics",
+    "Color": "Amarillo"
+  },
+  {
+    "nombre": "Casco de ciclismo",
+    "categoria": "Fitness",
+    "imagen": "deporte.png",
+    "codigo": "oya53ms724",
+    "precio": 335020,
+    "marca": "Puma",
+    "Color": "Naranja"
+  },
+  {
+    "nombre": "Cuerda para saltar",
+    "categoria": "Golf",
+    "imagen": "deporte.png",
+    "codigo": "suw76og344",
+    "precio": 323840,
+    "marca": "Decathlon",
+    "Color": "Naranja"
+  },
+  {
+    "nombre": "Rodillera",
+    "categoria": "Running",
+    "imagen": "deporte.png",
+    "codigo": "qud82pr814",
+    "precio": 440540,
+    "marca": "Puma",
+    "Color": "Verde"
+  },
+  {
+    "nombre": "Pelota de béisbol",
+    "categoria": "Running",
+    "imagen": "deporte.png",
+    "codigo": "mps62br450",
+    "precio": 148728,
+    "marca": "Nike",
+    "Color": "Gris"
+  },
+  {
+    "nombre": "Mat de yoga",
+    "categoria": "Ciclismo",
+    "imagen": "deporte.png",
+    "codigo": "ywe25so510",
+    "precio": 430631,
+    "marca": "Reebok",
+    "Color": "Gris"
+  },
+  {
+    "nombre": "Red de voleibol",
+    "categoria": "Running",
+    "imagen": "deporte.png",
+    "codigo": "toz54ad437",
+    "precio": 58055,
+    "marca": "Adidas",
+    "Color": "Amarillo"
+  },
+  {
+    "nombre": "Mancuernas",
+    "categoria": "Boxeo",
+    "imagen": "deporte.png",
+    "codigo": "ybi49cp948",
+    "precio": 444802,
+    "marca": "Under Armour",
+    "Color": "Amarillo"
+  },
+  {
+    "nombre": "Mat de yoga",
+    "categoria": "Running",
+    "imagen": "deporte.png",
+    "codigo": "agc38ej126",
+    "precio": 105808,
+    "marca": "Spalding",
+    "Color": "Celeste"
+  },
+  {
+    "nombre": "Balón de fútbol",
+    "categoria": "Ciclismo",
+    "imagen": "deporte.png",
+    "codigo": "ahy46vn221",
+    "precio": 217798,
+    "marca": "Reebok",
+    "Color": "Celeste"
+  },
+  {
+    "nombre": "Casco de ciclismo",
+    "categoria": "Ciclismo",
+    "imagen": "deporte.png",
+    "codigo": "riu81ha821",
+    "precio": 126935,
+    "marca": "Reebok",
+    "Color": "Naranja"
+  },
+  {
+    "nombre": "Palo de golf",
+    "categoria": "Golf",
+    "imagen": "deporte.png",
+    "codigo": "wgw22nq631",
+    "precio": 52684,
+    "marca": "Under Armour",
+    "Color": "Rojo"
+  },
+  {
+    "nombre": "Pelota de baloncesto",
+    "categoria": "Fútbol",
+    "imagen": "deporte.png",
+    "codigo": "hzh64nd978",
+    "precio": 390152,
+    "marca": "Everlast",
+    "Color": "Rojo"
+  },
+  {
+    "nombre": "Zapatillas deportivas",
+    "categoria": "Fútbol",
+    "imagen": "deporte.png",
+    "codigo": "jjq13fn471",
+    "precio": 482842,
+    "marca": "Everlast",
+    "Color": "Morado"
+  },
+  {
+    "nombre": "Rodillera",
+    "categoria": "Running",
+    "imagen": "deporte.png",
+    "codigo": "ylx19vw956",
+    "precio": 84747,
+    "marca": "Wilson",
+    "Color": "Blanco"
+  },
+  {
+    "nombre": "Gorro deportivo",
+    "categoria": "Fitness",
+    "imagen": "deporte.png",
+    "codigo": "ktv84gp177",
+    "precio": 72502,
+    "marca": "Wilson",
+    "Color": "Negro"
+  },
+  {
+    "nombre": "Cinta de correr",
+    "categoria": "Golf",
+    "imagen": "deporte.png",
+    "codigo": "bvy88mv550",
+    "precio": 192229,
+    "marca": "Adidas",
+    "Color": "Gris"
+  }
+];
+
+export default productos;
